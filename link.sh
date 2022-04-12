7251ef076b6936f846be2233889ff30ab18aad31
Sherman Rose <shermanrose@improbable.io>
2022-04-12 13:30:07 +0100

    Try and fix whitespacing in link

--- link.sh
+++ link.sh
@@ -25,8 +25,8 @@ link(){
 	ln -s "$1" "$2"
 }
 
-link "${DIR}/vim/vimrc"				"${HOME}/.vimrc"
-link "${DIR}/tmux/tmux.conf" 	"${HOME}/.tmux.conf"
+link "${DIR}/vim/vimrc"       "${HOME}/.vimrc"
+link "${DIR}/tmux/tmux.conf"  "${HOME}/.tmux.conf"
 link "${DIR}/git/gitconfig" 	"${HOME}/.gitconfig"
-link "${DIR}/zsh/zshrc"				"${HOME}/.zshrc"
-link "${DIR}/zsh/p10k.zsh"		"${HOME}/.p10k.zsh"
+link "${DIR}/zsh/zshrc"       "${HOME}/.zshrc"
+link "${DIR}/zsh/p10k.zsh"	  "${HOME}/.p10k.zsh"
