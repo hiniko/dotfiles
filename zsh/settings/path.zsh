23bb402af32321c2632b06d79842b4e9c2b2dda0
Sherman Rose <sherman@shermanrose.uk>
2024-12-01 17:32:51 +0000

    chore(zsh): added new path entrires

--- zsh/settings/path.zsh
+++ zsh/settings/path.zsh
@@ -1,3 +1,4 @@
+## TODO - Need to add a way to manage path between mac and linux
 
 # Add local gopath bin to path
 export GOPATH="${HOME}/go"
@@ -5,3 +6,10 @@ export PATH=$GOPATH/bin:$PATH
 
 # Add docker to path
 export PATH="$PATH:/Applications/Docker.app/Contents/Resources/bin"
+
+# Add .NET Core SDK tools
+export PATH="$PATH:/Users/sherman/.dotnet/tools"
+
+# Add Postgres to path
+export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
+
